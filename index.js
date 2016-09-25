819003671fbfe88c932f2dc3be1903bb30f79377
Alison <alisonhmullin@gmail.com>
2016-09-25 17:52:19 -0400

    complete initial state code-along

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much.";
+const ToggleButton = React.createClass({
+  getInitialState() {
+    return {
+      isEnabled: false
+    };
+  },
+
+  render() {
+    return (
+      <div className="toggle-button">I am toggled {this.state.isEnabled ? 'on' : 'off'}.</div>
+    );
+  }
+});
+
+class Address extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      fullAddress: `${props.street}, ${props.city}`
+    }
+  }
+
+  render() {
+    return (
+      <div className="address">{this.props.street}, {this.props.city}
+      </div>
+    );
+  }
+}
